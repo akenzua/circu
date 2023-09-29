fd02193ebc2dac08ecf403b852ec89e26b07c9ae
Akintade Ajibade <ajibade.akintade@gmail.com>
2023-09-29 13:56:18 +0100

    setup the app-shell and install @module-federation/nextjs-mf

--- packages/mold/next.config.js
+++ packages/mold/next.config.js
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const NextFederationPlugin = require('@module-federation/nextjs-mf')
 
-module.exports = nextConfig
+module.exports = {
+  webpack(config, options) {
+    const { webpack } = options
+    // if (!options.isServer) {
+    //   //config.cache=false
+    //   config.plugins.push(
+    //     new NextFederationPlugin({
+    //       name: 'checkout',
+    //       remotes: {},
+    //       filename: 'static/chunks/remoteEntry.js',
+    //       exposes: {},
+    //       shared: {},
+    //       extraOptions: {},
+    //     }),
+    //   )
+    // }
+
+    return config
+  },
+}
